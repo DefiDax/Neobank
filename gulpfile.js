5d5164917df03bb05e63c8b6be8faadd50d8c5a3
SpensDee <spensdee@gmail.com>
2021-02-21 12:14:55 +0200

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ function images() {
     return src('app/images/**/*.*')
     .pipe(imagemin([
         imagemin.gifsicle({interlaced: true}),
-        imagemin.mozjpeg({quality: 75, progressive: true}),
+        imagemin.mozjpeg({quality: 100, progressive: true}),
         imagemin.optipng({optimizationLevel: 5}),
         imagemin.svgo({
             plugins: [
